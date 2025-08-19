b3317e789ac2caa2d65da4b5a140826e9721bb9e
Álex <alex93cabeza@gmail.com>
2025-08-19 16:05:23 +0200

    test(kzgrs): Add benchmark for kzgrs' verify. (#1558)

--- nomos-da/kzgrs-backend/src/verifier.rs
+++ nomos-da/kzgrs-backend/src/verifier.rs
@@ -44,6 +44,9 @@ impl DaVerifier {
 
 #[cfg(test)]
 mod test {
+    #[cfg(target_arch = "x86_64")]
+    use std::hint::black_box;
+
     use nomos_core::da::{blob::Share as _, DaEncoder as _};
 
     use crate::{
@@ -62,6 +65,100 @@ mod test {
         for share in &encoded_data {
             let (light_share, commitments) = share.into_share_and_commitments();
             assert!(verifier.verify(&light_share, &commitments, domain_size));
+        }
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    mod utils {
+        use crate::encoder::DaEncoderParams;
+
+        pub struct Configuration {
+            pub label: String,
+            pub elements_count: usize,
+        }
+
+        impl Configuration {
+            pub const fn new(label: String, elements_count: usize) -> Self {
+                Self {
+                    label,
+                    elements_count,
+                }
+            }
+
+            pub fn from_elements_count(elements_count: usize) -> Self {
+                let label = format!("{elements_count}KiB");
+                let elements = elements_for_kib(elements_count);
+                Self::new(label, elements)
+            }
+        }
+
+        /// Returns the number of elements needed to generate `kib` KiB of data
+        /// before encoding. Assumes each element has size
+        /// [`DaEncoderParams::MAX_BLS12_381_ENCODING_CHUNK_SIZE`] (currently 31
+        /// bytes), as used by [`rand_data`](crate::encoder::test::rand_data).
+        ///
+        /// If either constant or function changes, this calculation must be
+        /// updated.
+        pub fn elements_for_kib(kib: usize) -> usize {
+            let bytes = kib * 1024;
+            bytes / DaEncoderParams::MAX_BLS12_381_ENCODING_CHUNK_SIZE
+        }
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    #[expect(
+        clippy::undocumented_unsafe_blocks,
+        reason = "This test is just to measure cpu and should be run manually"
+    )]
+    fn bench_verify_cycles(iters: u64, configuration: &utils::Configuration) {
+        let domain_size = 2048usize;
+        let encoder = DaEncoder::new(DaEncoderParams::default_with(domain_size));
+        let data = rand_data(configuration.elements_count);
+        let verifier = DaVerifier::new(GLOBAL_PARAMETERS.clone());
+        let encoded_data = encoder.encode(&data).unwrap();
+
+        let share = encoded_data.iter().next().unwrap();
+        let (light_share, commitments) = share.into_share_and_commitments();
+        let t0 = unsafe { core::arch::x86_64::_rdtsc() };
+        for _ in 0..iters {
+            black_box(verifier.verify(&light_share, &commitments, domain_size));
+        }
+        let t1 = unsafe { core::arch::x86_64::_rdtsc() };
+
+        let cycles_diff = t1 - t0;
+        let cycles_per_run = (t1 - t0) / iters;
+
+        let header = format!(
+            "=========== kzgrs-da-verify [{}] ===========",
+            configuration.label
+        );
+        let footer = "=".repeat(header.len());
+        println!("{header}",);
+        println!("  - iterations        : {iters:>20}");
+        println!(
+            "  - elements          : {:>20}",
+            configuration.elements_count
+        );
+        println!("  - cycles total      : {cycles_diff:>20}");
+        println!("  - cycles per run    : {cycles_per_run:>20}");
+        println!("{footer}\n");
+    }
+
+    // TODO: Remove this when we have the proper benches in the proofs
+    #[cfg(target_arch = "x86_64")]
+    #[ignore = "This test is just for calculation the cycles for the above set of proofs. This will be moved to the pertinent proof in the future."]
+    #[test]
+    fn test_verify_cycles() {
+        let iters = 1000u64;
+
+        let configurations = [
+            utils::Configuration::from_elements_count(32),
+            utils::Configuration::from_elements_count(256),
+            utils::Configuration::from_elements_count(1024),
+        ];
+
+        for configuration in &configurations {
+            bench_verify_cycles(iters, configuration);
         }
     }
 }
