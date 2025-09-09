a88965a3b889b77f809363f822408809235d27f7
Álex <alex93cabeza@gmail.com>
2025-09-09 11:35:56 +0200

    fix: Fix unresolved imports for find_binary. (#1663)

--- zk/proofs/poc/src/proving_key/mod.rs
+++ zk/proofs/poc/src/proving_key/mod.rs
@@ -4,6 +4,6 @@ const POC_PROVING_KEY_NAME: &str = "poc.zkey";
 const NOMOS_POC_PROVING_KEY_ENVAR: &str = "NOMOS_POC_PROVING_KEY_PATH";
 
 pub static POC_PROVING_KEY_PATH: LazyLock<PathBuf> = LazyLock::new(|| {
-    circuits_utils::find_binary(POC_PROVING_KEY_NAME, NOMOS_POC_PROVING_KEY_ENVAR)
+    circuits_utils::find_file(POC_PROVING_KEY_NAME, NOMOS_POC_PROVING_KEY_ENVAR)
         .unwrap_or_else(|_| panic!("{POC_PROVING_KEY_NAME} should not be missing"))
 });
